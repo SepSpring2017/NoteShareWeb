d3233432108e4aa528b035695ef6fb667b939d9d
Michael Codner <mhcodner@gmail.com>
2017-08-30 15:33:47 +1000

    Renamed Sign In to Register

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
