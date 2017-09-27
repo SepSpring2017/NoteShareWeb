50fbb7443835d976780d5d6d8ecf96fb91175d5f
Michael Codner <mhcodner@gmail.com>
2017-09-27 18:49:28 +1000

    Require auth for some routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,16 +9,16 @@ import { NoteComponent } from './note/note.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SearchComponent } from './search/search.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './_services/auth.guard';
 
 export const router: Routes = [
     { path: '', component: MainComponent, pathMatch: 'full'},
-    { path: 'main', component: MainComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'profile', component: ProfileComponent },
-    { path: 'search', component: SearchComponent },
-    { path: 'note', component: NoteComponent },
-    { path: 'register', component: RegisterComponent }
+    { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+    { path: 'search', component: SearchComponent, canActivate: [AuthGuard] },
+    { path: 'note', component: NoteComponent, canActivate: [AuthGuard] }
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
