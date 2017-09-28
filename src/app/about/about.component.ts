7b122cd5057ab9a8d0c91cf6274bef642aae8acf
David Omar Ruiz Torres <David.O.RuizTorres@student.uts.edu.au>
2017-09-28 13:20:22 +1000

    Minor aesthetics changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,7 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  templateUrl: './about.component.html',
+   // templateUrl: './about.component.html',
+  template: `<div class="container">
+            <div>
+              <h2 class="left-align">About</h2>
+            </div>
+          <p class="flow-text">
+            NoteShare is a site where you can upload your own notes (lecture notes, quiz answers, etc.) and share it with
+            other members.
+          </p>
+</div>
+`,
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
