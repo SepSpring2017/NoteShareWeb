fb033c76511591ef592fff60fa83e6de70b0f1aa
Michael Codner <mhcodner@gmail.com>
2017-10-12 17:04:32 +1100

    Remove unnecessary search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,6 @@ import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
-import { SearchComponent } from './search/search.component';
-import { NoteComponent } from './note/note.component';
 import { AboutComponent } from './about/about.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthenticationService } from './_services/authentication.service';
@@ -30,8 +28,6 @@ import { NotesComponent } from './notes/notes.component';
     HeaderComponent,
     LoginComponent,
     ProfileComponent,
-    SearchComponent,
-    NoteComponent,
     AboutComponent,
     MainComponent,
     RegisterComponent,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,8 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
-import { NoteComponent } from './note/note.component';
 import { NotesComponent } from './notes/notes.component';
 import { ProfileComponent } from './profile/profile.component';
-import { SearchComponent } from './search/search.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './_services/auth.guard';
 
@@ -18,9 +16,7 @@ export const router: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'about', component: AboutComponent },
     { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
-    { path: 'search', component: SearchComponent, canActivate: [AuthGuard] },
     { path: 'notes', component: NotesComponent, canActivate: [AuthGuard]},
-    { path: 'note', component: NoteComponent, canActivate: [AuthGuard] }
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
